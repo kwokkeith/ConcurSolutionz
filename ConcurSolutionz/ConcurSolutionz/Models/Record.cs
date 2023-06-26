e458841e9501e5fc709a2e81903316edf8c119e6
mrmrjing <hongjing123321@gmail.com>
2023-06-26 23:13:36 +0800

    Fixed Entry.cs

---
+++ ConcurSolutionz/ConcurSolutionz/Models/Record.cs
@@ -0,0 +1,12 @@
+﻿//using System;
+//namespace ConcurSolutionz
+//{
+//	public interface Record
+//	{
+//        int RecordId { get; }
+//        void AssignRecordId();
+//        int GetRecordId();
+//        void DelRecord();
+//    }
+//}
+
