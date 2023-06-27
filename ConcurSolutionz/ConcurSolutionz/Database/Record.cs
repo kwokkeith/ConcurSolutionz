5769c898e71e37f71e581cfa3e3d13ceb75a2ee4
Kwok Keith <79731084+kwokkeith@users.noreply.github.com>
2023-06-27 11:17:04 +0800

    Create Record.cs

---
+++ ConcurSolutionz/ConcurSolutionz/Database/Record.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConcurSolutionz.Database
+{
+    public class Record
+    {
+        
+    }
+}
