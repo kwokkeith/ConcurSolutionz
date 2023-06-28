3e508cfae7402a316f0375b5c07b3235d937b1b2
mrmrjing <hongjing123321@gmail.com>
2023-06-28 23:30:48 +0800

    Added functionality for Upload Receipt

--- ConcurSolutionz/ConcurSolutionz/Views/RecordPage.xaml.cs
+++ ConcurSolutionz/ConcurSolutionz/Views/RecordPage.xaml.cs
@@ -1,59 +1,76 @@
-﻿namespace ConcurSolutionz.Views;
-
-public partial class RecordPage : ContentPage
+﻿namespace ConcurSolutionz.Views
 {
-	public RecordPage()
-	{
-		InitializeComponent();
-	}
-
-    //public async Task<FileResult> PickandShow(PickOptions options)
-    //{
-    //    try
-    //    {
-    //        var result = await FilePicker.Default.PickAsync(options);
-    //        if (result != null)
-    //        {
-    //            if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
-    //                result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
-    //            {
-    //                using var stream = await result.OpenReadAsync();
-    //                var image = ImageSource.FromStream(() => stream);
-    //            }
-    //        }
-
-    //        return result;
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        // The user canceled or something went wrong
-            
-    //    }
-
-    //    return null;
-    //}
-
-    private async void OnFilePickerClicked(object sender, EventArgs e)
+    public partial class RecordPage : ContentPage
     {
-
-
-        var result = await FilePicker.PickAsync(new PickOptions
+        // Constructor
+        public RecordPage()
         {
-            FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>> { { DevicePlatform.iOS, new[] { ".png" } }, { DevicePlatform.macOS, new[] { ".png" } }, { DevicePlatform.MacCatalyst, new[] { ".png" } } }),
-            PickerTitle = "Select an image"
-        }); 
-
-        if(result == null)
-        {
-
-            return;
+            // Initialize the XAML components
+            InitializeComponent();
         }
 
-        var stream = await result.OpenReadAsync();
-        ReceiptImage.Source = ImageSource.FromStream(() => stream);
+        // Method to pick and show image file
+        public async Task<FileResult> PickAndShow(PickOptions options)
+        {
+            try
+            {
+                // Attempt to pick the file
+                var result = await FilePicker.PickAsync(options);
 
+                // If the result is not null, a file was picked
+                if (result != null)
+                {
+                    // If the picked file is a jpg or png
+                    if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
+                        result.FileName.EndsWith("jpeg", StringComparison.OrdinalIgnoreCase) ||
+                        result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Open a stream to the file
+                        var stream = await result.OpenReadAsync();
 
+                        // Set the ReceiptImage's source to the picked image
+                        ReceiptImage.Source = ImageSource.FromStream(() => stream);
+                    }
+                    else
+                    {
+                        // Not a supported image type
+                        await DisplayAlert("Error", "Selected file is not a supported image type", "OK");
+                    }
+                }
+                else
+                {
+                    // User canceled file picker
+                    return null;
+                }
 
+                // Return the file result
+                return result;
+            }
+            catch (Exception ex)
+            {
+                // The user canceled or something went wrong
+                await DisplayAlert("Error", "An error occurred while picking the file: " + ex.Message, "OK");
+
+                // Return null because an exception was thrown
+                return null;
+            }
+        }
+
+        // Event handler for the FilePicker button click event
+        private async void OnFilePickerClicked(object sender, EventArgs e)
+        {
+            // Call the PickAndShow method with the options for picking an image file
+            await PickAndShow(new PickOptions
+            {
+                FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>> {
+                    { DevicePlatform.iOS, new[] { ".jpg", ".jpeg", ".png" } },
+                    { DevicePlatform.macOS, new[] { ".jpg", "jpeg", ".png" } },
+                    { DevicePlatform.MacCatalyst, new[] { ".jpg", "jpeg", ".png" } },
+                    { DevicePlatform.Android, new[] { "image/*" } },
+                    { DevicePlatform.WinUI, new[] { ".jpg", "jpeg", ".png" } } 
+                }),
+                PickerTitle = "Select an image"
+            });
+        }
     }
-        
 }
