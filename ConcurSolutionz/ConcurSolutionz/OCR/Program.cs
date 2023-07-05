a321d1463cff9c8d021823b37afe6843401591b6
agx-hv <133509826+agx-hv@users.noreply.github.com>
2023-07-04 22:56:14 -0700

    Update Program.cs

--- ConcurSolutionz/ConcurSolutionz/OCR/Program.cs
+++ ConcurSolutionz/ConcurSolutionz/OCR/Program.cs
@@ -1,11 +1,6 @@
 #pragma warning disable 8600, 8602
 
 public class Program {
-    private static Char[] delimiters = new Char[] {' ','\t','\n'};
-    private static float accuracyMinPercent = 10.0F;
-    private static string[] keyWords = new string[] 
-    {"Total", "Amount", "Subtotal", "Grand Total", "Amount Due", "Tax", "Receipt", "Inv", "Invoice", "No", "Bill", "Slip"};
-
     public static void Main(String[] args) {
         ReceiptOCR r = new ReceiptOCR("../bin/1.jpg","../bin/tesseract");
         Console.WriteLine(r.receiptNumber);
