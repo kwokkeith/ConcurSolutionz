c084c24c897f9fcd45b203c4a19508cbd91ff5dc
refenir <66420170+refenir@users.noreply.github.com>
2023-07-19 02:34:10 +0800

    selenium path updated

--- ConcurSolutionz/ConcurSolutionz/Views/EntryPage.xaml.cs
+++ ConcurSolutionz/ConcurSolutionz/Views/EntryPage.xaml.cs
@@ -412,7 +412,11 @@ public partial class EntryPage : ContentPage
         {
 
             //process.StartInfo.FileName = AppDomain.CurrentDomain.BaseDirectory + "SeleniumWrapper\\SeleniumWrapper.exe";
-            process.StartInfo.FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "selenium", "SeleniumWrapperV2");
+            if (Directory.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "selenium")))
+            {
+                await DisplayAlert("nice", "you got it", "ok");
+            }
+            process.StartInfo.FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"..","..","..", "selenium", "SeleniumWrapperV2");
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardOutput = true;
             process.StartInfo.RedirectStandardInput = true;
